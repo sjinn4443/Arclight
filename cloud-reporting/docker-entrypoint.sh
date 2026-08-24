56da38506ef49320ba348f7cd488455ae824be65
sjinn4443 <sjinn4443@gmail.com>
2026-08-24 15:58:21 +0100

    Preserve report volume ownership across image updates

--- cloud-reporting/docker-entrypoint.sh
+++ cloud-reporting/docker-entrypoint.sh
@@ -3,6 +3,6 @@ set -eu
 
 umask 077
 mkdir -p "${REPORT_OUTPUT_DIR:-/data/reports}"
-chown arclight:arclight "${REPORT_OUTPUT_DIR:-/data/reports}"
+chown -R arclight:arclight "${REPORT_OUTPUT_DIR:-/data/reports}"
 exec gosu arclight python -m arclight_reporting
 
